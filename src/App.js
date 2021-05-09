b372823df433bbe09159d0e6b7d08c9db829f41d
shafiqraza <shafiqraza503@gmail.com>
2021-05-10 02:29:36 +0500

    re-structure folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { JumbotronContainer } from "./containers/jumbotron-container.component";
+// import { JumbotronContainer } from "./containers/jumbotron-container.component";
+import Jumbotron from "./components/jumbotron/jumbotron.component";
 
 function App() {
   return (
     <div>
-      <JumbotronContainer></JumbotronContainer>
+      <Jumbotron></Jumbotron>
+      {/* <JumbotronContainer></JumbotronContainer> */}
     </div>
   );
 }
--- src/components/jumbotron/jumbotron.component.jsx
+++ src/components/jumbotron/jumbotron.component.jsx
@@ -1,36 +1,30 @@
 import React from "react";
 
+import jumboData from '../../fixtures/jambo.json';
 
-import {  Inner, Item, Container, Pane, Title, SubTitle, Image } from "./styles/jumbotron.styles";
-
+import {  Inner, Item, Container, Pane, Title, SubTitle, Image } from "./jumbotron.styles";
 
 const Jumbotron = ({direction = "row", children, ...otherProps}) => {
   return (
-    <Item>
-      <Inner direction={direction}>{children}</Inner>
-    </Item>
+    <Container>
+      {
+        jumboData.map(item => (
+          <Item key={item.id}>
+        <Inner direction={direction}>
+          <Pane>
+            <Title>{ item.title }</Title>
+            <SubTitle>{item.subTitle}</SubTitle>
+          </Pane>
+          <Pane>
+            <Image src={item.image} alt={item.alt}></Image>
+          </Pane>
+        </Inner>
+      </Item>
+        ))
+      }
+      
+    </Container>
   );
-}
-
-
-Jumbotron.Container = ({children, ...otherProps}) => {
-  return <Container {...otherProps}>{ children }</Container>
-}
-
-Jumbotron.Pane = ({children, ...otherProps}) => {
-  return <Pane {...otherProps}>{ children }</Pane>
-}
-
-Jumbotron.Title = ({children, ...otherProps}) => {
-  return <Title {...otherProps}>{ children }</Title>
-}
-
-Jumbotron.SubTitle = ({children, ...otherProps}) => {
-  return <SubTitle {...otherProps}>{ children }</SubTitle>
-}
-
-Jumbotron.Image = ({...otherProps}) => {
-  return <Image {...otherProps} />
 }
 
 export default Jumbotron;
